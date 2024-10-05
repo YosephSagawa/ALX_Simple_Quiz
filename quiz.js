238e3ce12cae7f81304a11ef807c2ab89ceaa02e
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-10-05 17:50:28 +0300

    added the else condition

--- quiz.js
+++ quiz.js
@@ -5,7 +5,7 @@ function checkAnswer(){
     if(userAnswer === correctAnswer){
         const feedback = document.getElementById('feedback');
         feedback.textContent = "Correct! Well done.";
-    }else if(userAnswer !== correctAnswer){
+    }else{
         const feedback = document.getElementById('feedback');
         feedback.textContent = "That's incorrect. Try again!";
     }
