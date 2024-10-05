ad0d5e003822e3344531f39504bd3da1a923bdb4
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-10-05 16:33:42 +0300

    Added the simple calculator

---
+++ calculator.js
@@ -0,0 +1,43 @@
+function add(num1,num2){
+    return num1 + num2;
+}
+
+function sub(num1,num2){
+    return num1 - num2;
+}
+
+function mul(num1,num2){
+    return num1*num2;
+}
+
+function div(num1,num2){
+    return num1/num2;
+}
+
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('subtract').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = sub(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('multiply').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = mul(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('divide').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = div(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
--- quiz.js
+++ quiz.js
@@ -14,3 +14,4 @@ function checkAnswer(){
 const submitAnswer = document.getElementById('submit-answer');
 submitAnswer.addEventListener('click',checkAnswer);
 
+
