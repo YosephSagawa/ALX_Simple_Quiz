7c834001a833a215a9ab034fa42c7abdb5e01339
Yoseph Sagawa <yosephsagawa0@gmail.com>
2024-10-05 19:28:00 +0300

    Added comments

--- quiz.js
+++ quiz.js
@@ -3,7 +3,9 @@ function checkAnswer(){
     const radio = document.querySelector('input[name="quiz"]:checked');
     const userAnswer = radio.value; //This is used to access the value attribute of the selected radio button
     const feedback = document.getElementById('feedback');
-    if(userAnswer === correctAnswer){
+
+    //Add space before the if clause and the brackets to make it more readable andd to make it reviewable 
+    if (userAnswer === correctAnswer){ 
         feedback.textContent = "Correct! Well done.";
     }else{
         feedback.textContent = "That's incorrect. Try again!";
